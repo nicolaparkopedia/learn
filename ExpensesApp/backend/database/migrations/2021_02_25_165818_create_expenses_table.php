12e8e96b3ad6b01178ad6c633c800fad8e4de6e6
nicola <nicola@parkopedia.com>
2021-02-25 18:31:46 +0100

    tests

---
+++ ExpensesApp/backend/database/migrations/2021_02_25_165818_create_expenses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("description");
+            $table->float("amount");
+            $table->string("currency");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
---
+++ ExpensesApp/backend/routes/api.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Models\Expense;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get("/expenses", function (Request $request) {
+    return response(json_encode(Expense::all()),
+        200, ["Content-Type" => "application/json"]);
+});
+
+Route::post("/expenses", function (Request $request) {
+    $expense = Expense::factory([
+        "description" => $request->get("description", ""),
+        "amount" => $request->get("amount", ""),
+    ])->create();
+    return response(json_encode($expense),
+        201, ["Content-Type" => "application/json"]);
+});
+
+Route::get("/expenses/{id}", function (Request $request, $id) {
+    return response(json_encode(Expense::find($id)),
+        200, ["Content-Type" => "application/json"]);
+});
+
+Route::delete("/expenses/{id}", function (Request $request, $id) {
+    Expense::find($id)->delete();
+    return response(null,
+        200, ["Content-Type" => "application/json"]);
+});
+
+Route::patch("/expenses/{id}", function (Request $request, $id) {
+    $expense = Expense::find($id);
+    $expense->description = $request->get("description", $expense->description);
+    $expense->amount = $request->get("amount", $expense->amount);
+    $expense->save();
+
+    return response(json_encode($expense),
+        200, ["Content-Type" => "application/json"]);
+});
