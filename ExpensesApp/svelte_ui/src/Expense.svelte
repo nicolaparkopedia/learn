638eedcb49a148f41bdf85d77ff1c6a8ee057119
nicola <nicola@parkopedia.com>
2021-04-21 15:38:14 +0200

    Basic svelte app showing expenses from api

---
+++ ExpensesApp/svelte_ui/src/Expense.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let id
+    export let created_at
+    export let description
+    export let amount
+    export let currency
+
+    function handleClickExpense() {
+        alert(id);
+    }
+    
+</script>
+
+<tr on:click={handleClickExpense}>
+    <td>
+        {created_at}
+    </td>
+    <td>
+        {description}
+    </td>
+    <td>
+        {amount} {currency}
+    </td>
+</tr>
